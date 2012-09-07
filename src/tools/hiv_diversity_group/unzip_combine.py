759fceaa41ad93167b0041f55053eeabf92ccd54
nobubelo <nobubelo@mycompany.com>
2012-09-07 09:39:29 +0000

    edited unzip_combine to read both files and subdirectories in a zip archive
    git-svn-id: http://localhost:9000/svn/galaxy/uct_cbio_tools/trunk@62 2940091f-4021-4eef-aafe-5eb59827d404

--- src/tools/hiv_diversity_group/unzip_combine.py
+++ src/tools/hiv_diversity_group/unzip_combine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #uzip an archive and combine all file contents into one file
-#to run eg: python unzip_combine.py jinnynew.zip jinnynew_filelist
+#to run eg: python unzip_combine.py jinnynew.zip jinnynew_ALLsequences.fas
 import sys,os
 
 infile, outf =  sys.argv[-2], sys.argv[-1]
@@ -9,13 +9,19 @@ os.system(cmd)
 
 ndir = infile[:-4]+"_filelist"
 st = os.listdir(ndir)
+fpath = ndir+"/"
 for sst in st:
-	flist = os.walk(ndir+"/"+sst)
-	for fl in flist:
-		for i in range(len(fl[-1])):
-			fpath = fl[0]+"/"+fl[-1][i]
-			cmd = "cat "+fpath+" >> "+infile[:-4]+"_ALLsequences.fas"	
-			os.system(cmd)
+	if os.path.isdir(fpath+sst):
+		flist = os.walk(fpath+sst)
+		for fl in flist:
+			for i in range(len(fl[-1])):
+				fpath = fl[0]+"/"+fl[-1][i]
+				cmd = "cat "+fpath+" >> "+outf
+				os.system(cmd)
+	else:
+		ffpath = ndir+"/"+sst
+		cmd = "cat "+ffpath+" >> "+outf
+		os.system(cmd)
 
 rmc = "rm -R "+infile[:-4]+"_filelist"
 os.system(rmc)
